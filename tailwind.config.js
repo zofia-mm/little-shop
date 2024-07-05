c162068452cea2e76611534ef8c6722f4f7c13ef
Fobya7 <66226017+zofia-mm@users.noreply.github.com>
2024-07-05 22:03:18 +0200

    product list & details - basic styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,7 @@ module.exports =
 {
   content: [ "./src/**/*.{html,ts}", ],
   theme: {
-    extend: {
-      colors: {
-        primary: '#8492a6',
-        secondary: '#2692a6'
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
